18a700bb3325f0a768cce27fc909a5a73472be30
GabajMustafa <33008526+GabajMustafa@users.noreply.github.com>
2025-01-15 08:39:30 +0200

    Delete shareposts directory

--- shareposts/app/views/members/list.php
+++
@@ -1,5 +0,0 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1>All Members</h1>
-  <p>This is a social network type app built on the TraversyMVC framework</p>
-  <p><?php echo $data['Members']; ?></p>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
